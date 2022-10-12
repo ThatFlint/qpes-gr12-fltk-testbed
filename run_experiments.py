a443d633a4ed0ed45205bb8bece536aaf9c32bb0
JerritEic <J.D.Eickhoff@student.tudelft.nl>
2022-10-12 20:50:12 +0200

    Add partial results for 2^k

--- run_experiments.py
+++ run_experiments.py
@@ -257,7 +257,7 @@ def perform_sweep(configs):
 
 # ids of experiments to not run, eg. that have already been completed. Ids are in string above for reference.
 #DONT_RUN_MASK = [0,2,4,6,8,10,12,14]
-DONT_RUN_MASK = [0,1,2,3,4,5,6,7,8,9,10,11,13,15]
+DONT_RUN_MASK = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
 # Parameters
 pod_sizes = ['small', 'big']
